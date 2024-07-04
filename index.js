c2f3390d75f9567a6b041473700cec901da7752d
Riday1 <rrriday100@gmail.com>
2024-07-04 22:08:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.w8wev2h.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+
+
+    try {
+
+
+    } finally {
+
+    }
+}
+run().catch(err => console.log(err));
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('server is running')
+})
+app.listen(port, () => {
+    console.log(`server is running on port : ${port}`)
+})
+
+
+
+// ServiceProvider:Yku7JQftvxoOGfpy
